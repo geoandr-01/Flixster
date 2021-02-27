4e7aa575c57223a5bf1a275676578e37bb0f785c
geoandr-01 <geoandr@live.unc.edu>
2021-02-26 22:13:47 -0500

    update

---
+++ flixter/MovieGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridViewCell.swift
+//  flixter
+//
+//  Created by George Andrade on 2/26/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
